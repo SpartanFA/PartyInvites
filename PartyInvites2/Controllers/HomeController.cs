7cb451ccff2faa4cef5217318b9794ebb22c14b4
Benjamin Werner <bewerner23@gmail.com>
2022-08-25 17:53:26 -0400

    Init

---
+++ PartyInvites2/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites2.Models;
+
+namespace PartyInvites2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            return View();
+        }
+
+        public ViewResult Gallery()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ PartyInvites2/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites2.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your age")]
+
+        public string Age { get; set; }
+
+        [Required(ErrorMessage = "Please enter your arrival time")]
+        public string ArrivalTime { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you require parking assitance")]
+
+        public bool? WillAttend { get; set; }
+    }
+}
